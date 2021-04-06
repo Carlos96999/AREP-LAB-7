0af0bbe9b353c877481e629f20f4f9bdd959b50b
Carlos Amorocho <allblacksc@hotmail.com>
2021-04-06 17:46:19 -0500

    Añadido JavaDoc

--- src/main/java/edu/escuelaing/arep/App.java
+++ src/main/java/edu/escuelaing/arep/App.java
@@ -7,6 +7,10 @@ import static spark.Spark.get;
 import static spark.Spark.port;
 import static spark.Spark.staticFileLocation;
 
+/*
+ * Servidor sparkweb para iniciar el servicio Convertir grados Fahrenheit a Celsius
+ * Autor: Carlos Amorocho
+ */
 public class App 
 {
 	public static ConvertirGrados convertir = new ConvertirGrados();
@@ -19,6 +23,10 @@ public class App
         get("/", (req, res) -> {res.redirect("index.html"); return "";});
     }
         
+	/*
+	 * Clase que llama a la aplicacion para convertir el valor
+	 * @Return Devuelve un objeto JSON con los datos y la operacion realizada
+	 */
     private static Object celsius(Request request, Response response) 
     {
         Double value = Double.valueOf(request.queryParams("value"));
@@ -29,6 +37,10 @@ public class App
         return myObject;
     }
     
+    /*
+     * Obtenemos un puerto para el uso de nuestro servicio, si no se genera automaticamente se asigna uno
+     * @Return retorna un puerto por defecto "5000"
+     */
     public static int getPort()
     {
         if (System.getenv("PORT") != null) 
--- src/main/java/edu/escuelaing/arep/ConvertirGrados.java
+++ src/main/java/edu/escuelaing/arep/ConvertirGrados.java
@@ -1,8 +1,17 @@
 package edu.escuelaing.arep;
 
+/*
+ * Clase que se encarga de realizar la operacion de conversion
+ * Autor: Carlos Amorocho
+ */
 public class ConvertirGrados 
 {
 
+	/*
+	 * Realiza la conversion a grados Celsius
+	 * @Param fahrenheit Valor en grados Fahrenheit que se desea convertir
+	 * @Return Devuelve el respectivo valor en grados Celsius
+	 */
 	public static double celsius(double fahrenheit)
 	{
 		double celsius = (fahrenheit-32)*(0.5556);
